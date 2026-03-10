e6d2fdf4557e69f594158ead3256cc3e8dac54ba
finotremy-a11y <finotremy@gmail.com>
2026-03-10 11:15:56 +0100

    fix: Force RAILS_ENV=production in render-build script to prevent sqlite3 LoadError

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,11 @@
 # exit on error
 set -o errexit
 
+# Force RAILS_ENV to production during build
+export RAILS_ENV=production
+export RAILS_MASTER_KEY=${RAILS_MASTER_KEY}
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-
 bundle exec rails db:migrate
