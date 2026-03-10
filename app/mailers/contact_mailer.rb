82869f6b9c109b2bac4b01ebd2af81428faa03fa
finotremy-a11y <finotremy@gmail.com>
2026-03-10 12:50:56 +0100

    fix: Use contact@revesdev.fr as sender with reply-to visitor email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,8 +4,9 @@ class ContactMailer < ApplicationMailer
   def new_contact_email
     @contact = params[:contact]
     mail(
-      from: @contact.email,
-      subject: "Nouveau Message de: #{@contact.name} - #{@contact.subject.presence || 'Sans sujet'}"
+      from:     "RêvesDev Contact <contact@revesdev.fr>",
+      reply_to: @contact.email,
+      subject:  "Nouveau Message de: #{@contact.name} - #{@contact.subject.presence || 'Sans sujet'}"
     )
   end
 end
