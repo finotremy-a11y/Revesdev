37cd124502b3c304f90d0dab721ed4e8503f33a0
finotremy-a11y <finotremy@gmail.com>
2026-03-10 10:16:10 +0100

    fix: Resolve Brakeman XSS cross-site scripting warning on project links

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,10 @@
 module ProjectsHelper
+  def safe_url(url)
+    return nil if url.blank?
+    
+    uri = URI.parse(url)
+    uri.is_a?(URI::HTTP) ? uri.to_s : nil
+  rescue URI::InvalidURIError
+    nil
+  end
 end
